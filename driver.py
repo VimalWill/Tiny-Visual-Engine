627a5dc160a04d3e71eaaa4bd28ad95042e1f467
vimal <vimalwilliam99@gmail.com>
2023-03-08 19:09:21 +0530

    modified the driver

--- driver.py
+++ driver.py
@@ -1,43 +1,42 @@
-import Jetson.GPIO as GPIO
+import RPi.GPIO as GPIO 
 
 class MotorDriver:
-    def __init__(self, motor_pins, enable_pins):
-        self.motor_pins = motor_pins
-        self.enable_pins = enable_pins
-        self.GPIO = GPIO
+    def __init__(self, en1, en2, in1, in2, in3, in4) -> None:
 
-        # Set up GPIO pins
-        self.GPIO.setmode(GPIO.BOARD)
-        for pin in self.motor_pins + self.enable_pins:
-            self.GPIO.setup(pin, self.GPIO.OUT)
+        #pin init for the motor and en 
+        self.en1 = en1 
+        self.en2 = en2 
+        self.in1 = in1 
+        self.in2 = in2 
+        self.in3 = in3 
+        self.in4 = in4 
 
-        # Initialize PWM objects
-        self.motors = []
-        for pin in self.enable_pins:
-            self.motors.append(self.GPIO.PWM(pin, 100))
-            self.motors[-1].start(0)
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.en1, GPIO.OUT)
+        GPIO.setup(self.en2, GPIO.OUT)
+        GPIO.setup(self.in1, GPIO.OUT)
+        GPIO.setup(self.in2, GPIO.OUT)
+        GPIO.setup(self.in3, GPIO.OUT)
+        GPIO.setup(self.in4, GPIO.OUT)
 
-    def set_direction(self, in1, in2, direction):
-        if direction == "forward":
-            self.GPIO.output(in1, self.GPIO.HIGH)
-            self.GPIO.output(in2, self.GPIO.LOW)
-        elif direction == "reverse":
-            self.GPIO.output(in1, self.GPIO.LOW)
-            self.GPIO.output(in2, self.GPIO.HIGH)
+        self.mp_1 = GPIO.PWM(self.en1, 1000)
+        self.mp_2 = GPIO.PWM(self.en2, 1000)
+
+    def run(self, val):
+        self.mp_1.start(25)
+        self.mp_2.start(25)
+        if val:
+            GPIO.output(self.in1, GPIO.HIGH)
+            GPIO.output(self.in2, GPIO.LOW)
+
+            GPIO.output(self.in3, GPIO.HIGH)
+            GPIO.output(self.in4, GPIO.LOW)
+        
         else:
-            raise ValueError("Invalid direction: {}".format(direction))
+            GPIO.output(self.in1, GPIO.LOW)
+            GPIO.output(self.in2, GPIO.LOW)
 
-    def set_speed(self, speed):
-        for motor in self.motors:
-            motor.ChangeDutyCycle(speed)
+            GPIO.output(self.in3, GPIO.LOW)
+            GPIO.output(self.in4, GPIO.LOW)
+            
 
-    def start_stop(self, start):
-        if start:
-            for motor in self.motors:
-                motor.start(50)
-        else:
-            for motor in self.motors:
-                motor.stop()
-
-    def cleanup(self):
-        self.GPIO.cleanup()
--- main.py
+++ main.py
@@ -3,11 +3,7 @@ import cv2
 from driver import MotorDriver
 import threading
 
-motor_pins = [3, 5, 7, 8]  # IN1 and IN2 pins for each motor
-enable_pins = [37, 38]  # Enable pins for each motor
-md = MotorDriver(motor_pins, enable_pins)
-
-
+md = MotorDriver(37, 38, 3, 5, 7, 8)
 
 if __name__ == "__main__":
     #load the model 
@@ -18,9 +14,9 @@ if __name__ == "__main__":
         img_det, conf = infer(frame, infer_se)
         if not len(conf) == 0:
             if(conf[-1] >= 70):
-                md.start_stop(False)
+                md.run(True)
             else:
-                md.start_stop(True)
+                md.run(False)
 
         cv2.imshow("",img_det)
         cv2.waitKey(1)
